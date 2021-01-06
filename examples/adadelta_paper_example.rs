6c20a95e85c85a8791ca6bfa4a0a3e0538461b07
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-01-06 23:06:33 +0000

    temp2

---
+++ examples/adadelta_paper_example.rs
@@ -0,0 +1,5 @@
+// This example reproduces the network described in Zeiler, M.D., Adadelta: An Adaptive Learning Rate Method, arXiv:1212.5701v1, 2012.
+// An accuracy of about 97.4% is achieved on the test set.
+
+use neuro::activations::Activation;
+use neuro::d

931c19ddb63ddbf947e0b9ee20e3ac5d8e60bbe4
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-01-14 14:27:58 +0000

    Reformatted the code.

--- examples/feedforward.rs
+++ examples/feedforward.rs
@@ -10,4 +10,25 @@ use neuro::tensor::*;
 use std::path::Path;
 
 
-fn main() -> Resu
+fn main() -> Result<(), Error> {
+
+    // Load the data
+    let inputs = Path::new("datasets/tabular_data/input_normalized.csv");
+    let outputs = Path::new("datasets/tabular_data/single_output_normalized.csv");
+    let data = TabularDataSet::from_csv(&inputs, &outputs, 0.1, true)?;
+    println!("{}", data);
+
+    // Create the network
+    let mut nn = Network::new(data.input_shape(), losses::MeanSquaredError::new(), Adam::new(0.01), None)?;
+    nn.add(Dense::new(32, Activation::ReLU));
+    nn.add(Dense::new(16, Activation::ReLU));
+    nn.add(Dense::new(1, Activation::Linear));
+    println!("{}", nn);
+
+    // Train and save the model
+    nn.fit(&data, 64, 50, Some(10), None);
+    nn.save("feedforward.h5")?;
+
+    // Predictions: create two inputs: (-0.5, 0.92, 0.35) and (0.45, -0.72, -0.12).
+    let inputs = Tensor::new(&[-0.5, 0.92, 0.35, 0.45, -0.72, -0.12], Dim::new(&[3, 1, 1, 2]));
+    let res = nn.predict(&inputs); 

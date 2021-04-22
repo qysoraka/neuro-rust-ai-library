942dfd2f53b7e4f372c85b379ccb167e61f6bf22
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-04-22 08:22:28 +0000

    update 3 files: biblio-numeric-suffixes.json, biblio/biblio-ed.data, biblio/biblio-wg.data

--- src/io.rs
+++ src/io.rs
@@ -9,4 +9,24 @@ use crate::tensor::*;
 /// # Arguments
 ///
 /// * `file` - The file in which  the group is created.
-/// * `group_name` - Th
+/// * `group_name` - The name of the group.
+pub(crate) fn create_group(file: &hdf5::File, group_name: &str) -> hdf5::Group {
+    let name = CString::new(group_name).unwrap();
+    unsafe {
+        let lcpl = hdf5_sys::h5p::H5Pcreate(*H5P_CLS_LINK_CREATE);
+        let gcpl = hdf5_sys::h5p::H5Pcreate(*H5P_CLS_GROUP_CREATE);
+        hdf5_sys::h5p::H5Pset_link_creation_order(gcpl, H5P_CRT_ORDER_TRACKED | H5P_CRT_ORDER_INDEXED);
+        hdf5_sys::h5g::H5Gcreate2(file.id(), name.as_ptr(), lcpl, gcpl, H5P_DEFAULT);
+    }
+    file.group(group_name).expect("Could not create the group.")
+
+}
+
+/// Lists the subgroups contained in a group.
+///
+/// # Return value
+///
+/// Vector containing the names of the subgroups, listed by creation order.
+pub(crate) fn list_subgroups(group: &hdf5::Group) -> Vec<String> {
+    extern "C" fn members_callback(
+        _id: hdf5_sys::h5i::hid_t, name: *const std::os::raw::c_char, _info: *const hdf5_sys::h5l::H5L_info_t, op_data: 

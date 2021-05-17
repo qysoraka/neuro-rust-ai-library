05d7215eb2e5449c8bad00a6ce6fc795ae072ae1
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-05-17 09:48:10 +0000

    Updated pl translation

--- src/layers/conv2d.rs
+++ src/layers/conv2d.rs
@@ -42,4 +42,32 @@ pub struct Conv2D {
     stride: (u64, u64),
     padding: Padding,
     padding_size: (u64, u64, u64, u64), // top, right, bottom, left
-    num_fil
+    num_filters: u64,
+    input_shape: Dim,
+    output_shape: Dim,
+    weights: Tensor,
+    biases: Tensor,
+    dweights: Tensor,
+    dbiases: Tensor,
+    linear_activation: Option<Tensor>,
+    previous_activation: Option<Tensor>,
+    reshaped_input: Tensor,
+    weights_initializer: Initializer,
+    biases_initializer: Initializer,
+    regularizer: Option<Regularizer>,
+}
+
+impl Conv2D {
+
+    pub(crate) const NAME: &'static str = "Conv2D";
+
+    /// Creates a 2D convolution layer with the given parameters.
+    ///
+    /// By default, a ReLU activation is used and the parameters of the kernels are initialized
+    /// using a HeNormal initializer and the biases of the layer a Zeros initializer.
+    ///
+    /// # Arguments
+    ///
+    /// * `num_filters` - The number of filters in the layer.
+    /// * `kernel_size` - The height and width of the convolution kernels.
+    /// * `stride` - The vertical and horizonta

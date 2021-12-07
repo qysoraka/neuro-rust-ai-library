cec97ff8e770857e51c1a4090ecfb4c62f5417fe
qysoraka <156439778+qysoraka@users.noreply.github.com>
2021-12-07 17:09:12 +0000

    profile fixes

--- src/layers/dense.rs
+++ src/layers/dense.rs
@@ -37,4 +37,27 @@ impl Dense
 
     /// Creates a dense layer with given number of units and activation function.
     ///
-    /// By default, the weights are initialized with a HeUniform initializer
+    /// By default, the weights are initialized with a HeUniform initializer and the biases with a Zeros initializer.
+    pub fn new(units: u64, activation: Activation) -> Box<Dense> {
+        Box::new(Dense {
+            units,
+            activation,
+            weights: Tensor::new_empty_tensor(),
+            dweights: Tensor::new_empty_tensor(),
+            biases: Tensor::new_empty_tensor(),
+            dbiases: Tensor::new_empty_tensor(),
+            input_shape: Dim::new(&[0, 0, 0, 0]),
+            output_shape: Dim::new(&[units, 1, 1, 1]),
+            linear_activation: None,
+            previous_input: None,
+            weights_initializer: Initializer::HeNormal,
+            biases_initializer: Initializer::Zeros,
+            regularizer: None,
+        })
+    }
+
+    /// Creates a dense layer with the given parameters.
+    pub fn with_param(units: u64,
+                      activation: Activation,
+                      weights_initializer: Initializer,
+                      biase

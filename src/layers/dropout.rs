0284fd783f32cf293a3b1b35c9de7951ef3cd88a
qysoraka <156439778+qysoraka@users.noreply.github.com>
2022-05-03 15:40:23 +0000

    dcache: Handle escaped paths in prepend_path

--- src/layers/dropout.rs
+++ src/layers/dropout.rs
@@ -61,4 +61,36 @@ impl Dropout {
     pub(crate) fn from_hdf5_group(group: &hdf5::Group) -> Box<Self> {
         let _ = hdf5::silence_errors();
         let drop_rate = group.dataset("drop_rate").and_then(|ds| Ok(read_scalar::<f64>(&ds))).expect("Could not retrieve the drop rate.");
-        let output_shape = group.dataset("output_shape").and_then(|value| value.read_raw::<[u64; 4
+        let output_shape = group.dataset("output_shape").and_then(|value| value.read_raw::<[u64; 4]>()).expect("Could not retrieve the output shape.");
+
+        let mut rng = rand::thread_rng();
+        let seed: u64 = rng.gen();
+        let random_engine = RandomEngine::new(RandomEngineType::PHILOX_4X32_10, Some(seed));
+
+        let scaling_factor = 1. / (1. - drop_rate) as PrimitiveType;
+
+        Box::new(Self {
+            drop_rate,
+            output_shape: Dim::new(&(output_shape[0])),
+            grad: Tensor::new_empty_tensor(),
+            random_engine,
+            scaling_factor,
+        })
+    }
+}
+
+impl Layer for Dropout {
+    fn name(&self) -> &str {
+        Self::NAME
+    }
+
+    fn initialize_parameters(&mut self, input_shape: Dim4) {
+        self.output_shape = input_shape;
+    }
+
+    fn compute_activation(&self, prev_activation: &Tensor) -> Tensor {
+        prev_activation.copy()
+    }
+
+    fn compute_activation_mut(&mut self, prev_activation: &Tensor) -> Tensor {
+        let mask = self.gener
